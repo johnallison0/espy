d3bb3d82d9225e86b0426e66b5e86df248b005de
John Allison <25298104+johnallison0@users.noreply.github.com>
2019-06-14 16:03:36 +0100

    Reads no. of zones for energy balance and overheating. Fixes CSV output in res_get.

--- espy/module1.py
+++ espy/module1.py
@@ -454,7 +454,16 @@ def gen_qa_report(cfg_file, filename):
 
 
 def res_get(cfg_file, res_file, out_file, param_list, time_fmt):
-    """Extract results from results database to CSV."""
+    """Extract results from results database to CSV.
+
+    TODO(j.allison): if more than 42 this will break.
+    
+    param_list can be in the following format:
+    param_list = [['all', 'Zone db T']]
+    param_list = [['id:reception', 'Zone db T']]
+    param_list = [[['id:roof_space', 'id:reception'], 'Zone db T']]
+    param_list = [[['a', 'b'], 'Zone db T'], [['id:reception', 'b'], 'Wind direction']]
+    """
     res_dict = {
         # Climate
         "Ambient temperature": ["a", "a"],
@@ -465,18 +474,18 @@ def res_get(cfg_file, res_file, out_file, param_list, time_fmt):
         "Ambient RH": ["a", "f"],
         "Sky illuminance": ["a", "g"],
         # Temperatures
-        "Zone db T": ["b", "a"],
-        "Zone db T - ambient db T": ["b", "b"],
-        "Zone db T - other zone db T": ["b", "c"],
-        "Zone control point T": ["b", "d"],
-        "Zone Resultant T": ["b", "e"],
-        "Mean Radiant T (area wtd)": ["b", "f"],
-        "Mean Radiant T (at sensor)": ["b", "g"],
-        "Dew point T": ["b", "h"],
-        "Surf inside face T": ["b", "i"],
-        "Surf T - dewpoint T": ["b", "j"],
-        "Surf outside face T": ["b", "k"],
-        "Surf node T": ["b", "l"],
+        "Zone db T": ["b", "a", "-"],
+        "Zone db T - ambient db T": ["b", "b", "-"],
+        "Zone db T - other zone db T": ["b", "c", "-"],
+        "Zone control point T": ["b", "d", "-"],
+        "Zone Resultant T": ["b", "e", "-"],
+        "Mean Radiant T (area wtd)": ["b", "f", "-"],
+        "Mean Radiant T (at sensor)": ["b", "g", "-"],
+        "Dew point T": ["b", "h", "-"],
+        "Surf inside face T": ["b", "i", "-"],
+        "Surf T - dewpoint T": ["b", "j", "-"],
+        "Surf outside face T": ["b", "k", "-"],
+        "Surf node T": ["b", "l", "-"],
         # Comfort metrics
         # <TBC> requires extra inputs from user
         # 'Predicted Mean Vote (PMV)': ['c', 'a'],
@@ -518,6 +527,8 @@ def res_get(cfg_file, res_file, out_file, param_list, time_fmt):
         "Aggregate cooling load": ["h", "i"],
         "Aggregate dehumidification": ["h", "j"],
         "Aggregate humidification": ["h", "k"],
+        # Zone RH
+        "Zone RH": ["i"],
     }
 
     # Read cfg file for list of zones
@@ -600,10 +611,30 @@ def res_get(cfg_file, res_file, out_file, param_list, time_fmt):
     res = run(
         ["res", "-file", res_file, "-mode", "script"], stdout=PIPE, input=cmd, encoding="ascii"
     )
+
+    # Trim comment from header of file
+    with open(out_file, "r") as infile, open("my" + out_file, "w", newline="") as outfile:
+        reader = csv.reader(infile)
+        writer = csv.writer(outfile)
+        line_count = 1
+        for row in reader:
+            if line_count == 4:
+                newrow = ["Time"] + row[1:]
+            else:
+                newrow = row
+            writer.writerow(newrow)
+            line_count += 1
+
+
+
     return res
 
 
-def get_overheating_stats(res_file, out_file, query_point=25):
+def get_overheating_stats(cfg_file, res_file, out_file, query_point=25):
+    # Read cfg file for list of zones
+    _, _, _, _, _, _, zones = read_cfg(cfg_file)
+    
+    # Get overheating stats from ESP-r to temporary file
     cmd = ["", "d", ">", "temp.csv", "", "^", "e", "c", "b", "a", "-", str(query_point), ">", "-", "-"]
     cmd = "\n".join(cmd)
     run(
@@ -613,8 +644,6 @@ def get_overheating_stats(res_file, out_file, query_point=25):
     # Read in CSV output from ESP-r
     data = []
     header = 9
-    #TODO(j.allison): Get number of zones from cfg file
-    zones = 2
     with open("temp.csv", "r") as file:
         reader = csv.reader(file, delimiter=",")
         line_count = 1
@@ -622,14 +651,18 @@ def get_overheating_stats(res_file, out_file, query_point=25):
             if line_count < header:
                 # skipping header
                 line_count += 1
-            elif line_count >= header + zones:
+            elif line_count >= header + len(zones):
                 break
             else:
                 data.append(row)
                 line_count += 1
 
     # remove temporary CSV file
-    os.remove("temp.csv")
+    # Handle errors while calling os.remove()
+    try:
+        os.remove("temp.csv")
+    except:
+        print("Error while deleting file ", "temp.csv")
     
     # Calculate total number of hours
     total_hours = float(data[0][6]) + float(data[0][7])
@@ -638,7 +671,7 @@ def get_overheating_stats(res_file, out_file, query_point=25):
     # print(data)
     overheating_frequency = []
     for zone in data:
-        overheating_frequency.append([zone[0], round(float(zone[6])/total_hours*100,1)])
+        overheating_frequency.append([zone[0], round(float(zone[6])/total_hours*100, 1)])
 
     # print(".")
     # Write back out to CSV that can be parsed by HighCharts
@@ -651,7 +684,11 @@ def get_overheating_stats(res_file, out_file, query_point=25):
     return overheating_frequency
 
 
-def get_energy_balance(res_file, out_file=None):
+def get_energy_balance(cfg_file, res_file, out_file=None):
+    # Read cfg file for list of zones
+    _, _, _, _, _, _, zones = read_cfg(cfg_file)
+
+    # Get zone energy balance from ESP-r to temporary file
     cmd = ["", "d", ">", "temp.csv", "", "^", "e", "h", "b", "b", ">", "-", "-"]
     cmd = "\n".join(cmd)
     run(
@@ -660,30 +697,33 @@ def get_energy_balance(res_file, out_file=None):
 
     # Read CSV from ESP-r
     data = []
-    #TODO(j.allison): Get number of zones from cfg file
-    zones = 2
-    for i in range(zones):
+    for i in range(len(zones)):
         with open("temp.csv", "r") as file:
             reader = csv.reader(file, delimiter=",")
             data.append([row for idx, row in enumerate(reader) if idx in range(19*i+6, 19*i+21)])
 
     # remove temporary CSV file
-    os.remove("temp.csv")
+    # os.remove("temp.csv")
 
     # Restructure data for HighCharts
+    # Assume all headers in first zone. If no plant input/extract this will not work
+    # for the last two items
     headers = ["Stack"] + [x[0].strip() for x in data[0]]
     zone_gains = []
     zone_losses = []
     for zone in data:
-        zone_gains.append([float(x[1]) for x in zone])
-        zone_losses.append([float(x[2]) for x in zone])
-
-    # Add across all zones
+        # ESP-r bug: 'Convection @ transp surf' written out as 'No plant input/extract'
+        # when there is 'No plant input/extract' i.e. zone[12] will be length 1.
+        # Also, when 'No plant input/extract' i.e. zone[13] will be length 1.
+        zone_gains.append([float(x[1]) if len(x) == 3 else 0 for x in zone])
+        zone_losses.append([float(x[2]) if len(x) == 3 else 0 for x in zone])
+        
+    # Sum across all zones
     total_gains = ["Gain"] + [round(sum(x), 1) for x in zip(*zone_gains)]
     total_losses = ["Loss"] + [abs(round(sum(x), 1)) for x in zip(*zone_losses)]
 
-    # Export to HighCharts CSV format
-    if out_file != None:
+    # Export to HighCharts CSV format if given out_file
+    if out_file is not None:
         with open(out_file, "w", newline="") as write_file:
             writer = csv.writer(write_file)
             writer.writerow(headers[0:-1])
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
+"""Setup file for installing module."""
 from setuptools import setup
 
 with open("README.md", 'r') as f:
-    long_description = f.read()
+    long_description = f.read()  # pylint: disable=invalid-name,locally-disabled
 
 setup(
     name='espy',
@@ -12,15 +12,4 @@ setup(
     author_email='',
     packages=['espy'],  #same as name
     install_requires=['datetime', 'numpy', 'matplotlib'], #external packages as dependencies
-=======
-from setuptools import setup
-
-setup(
-   name='espy',
-   version='0.1',
-   description='Python API for ESP-r',
-   author='John Allison',
-   packages=['espy'],  #same as name
-   install_requires=['datetime', 'numpy', 'matplotlib'], #external packages as dependencies
->>>>>>> 36945ab2eb3a9b109812b2bcba3d9b0f5616347d
 )
